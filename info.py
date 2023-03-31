b739ff586b5cc2da4b6e6fb8fee47bc0ff39bb35
Kutyponnu <123724064+Kutyponnu@users.noreply.github.com>
2023-03-31 15:05:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,8 +59,8 @@ G_FILTER = bool(environ.get("G_FILTER", True))
 BUTTON_LOCK = environ.get("BUTTON_LOCK", "True")
 
 # url shortner
-SHORT_URL = environ.get("SHORT_URL")
-SHORT_API = environ.get("SHORT_API")
+SHORT_URL = environ.get("omegalinks.in")
+SHORT_API = environ.get("0ffce356f0dc1ce0b37703468078765181aeca0a")
 
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
